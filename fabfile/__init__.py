e9d3a563cf8d104b02fca67c75e5f80842f4415e
Helio Tejedor <heltena@gmail.com>
2017-12-05 17:28:00 -0600

    - initial commit - django skel - simple api with sql model

---
+++ fabfile/__init__.py
@@ -0,0 +1,16 @@
+from fabric.api import cd, env, task, local, run, settings
+import json
+import requests
+    
+@task
+def timeseries():
+    value = {
+        "dataset": "mouse_aging", 
+        "xaxis": "age",
+        "series": "gene",
+        "restrictions": [
+            ["tissue", "in", ["AM", "LUNG"]],
+            ["flu", "eq", 150],
+            ["gene", "in", ["ENSMUSG00000000088", "ENSMUSG00000000001"]]]}
+    r = requests.post("http://127.0.0.1:8000/api/timeseries", json=value)
+    print("Result: {}".format(r.json()))
---
+++ webapp/api/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+from django.core.serializers.json import DjangoJSONEncoder
+from django.views.decorators.csrf import csrf_exempt
+
+from api.tags import method
+from collections import defaultdict
+import json
+import numpy as np
+
+
+class NumpyEncoder(DjangoJSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, np.integer):
+            return int(obj)
+        elif isinstance(obj, np.floating):
+            return float(obj)
+        elif isinstance(obj, np.ndarray):
+            return obj.tolist()
+        else:
+            return super(NumpyEncoder, self).default(obj)
+
+
+@csrf_exempt
+@method(allowed=['POST'])
+def time_series(request):
+    body = json.loads(request.body.decode("utf-8"))
+    xaxis = body.get("xaxis", None)
+    series = body.get("series", None)
+    restrictions = body.get("restrictions", [])
+    
+    if xaxis is None:
+        result = {"ok": False,
+                  "message": "xaxis not valid"}
+        return JsonResponse(result)
+
+    if series is None:
+        result = {"ok": False,
+                  "message": "series not valid"}
+        return JsonResponse(result)
+
+    return JsonResponse({"ok": True})
